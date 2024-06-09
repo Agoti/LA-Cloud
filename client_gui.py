9aee5c0cb0482fd545e376995fa859288051637a
alpttex19 <alpttex19@mails.tsinghua.edu.cn>
2024-06-09 17:55:44 +0800

    client_gui: move RawClient into this func

--- client_gui.py
+++ client_gui.py
@@ -4,11 +4,28 @@ import os
 from PIL import Image, ImageTk  # 导入PIL
 from IO.IOStream import *
 from Constants import *
-from RawClient import RawClient
 import struct
 import time
 import threading
 
+class RawClient:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.knock = Knock(method='socket', host=host, port=port)
+        self.io_stream = self.knock.knock()
+
+    def send(self, data, is_byte = False):
+        print(f"Sending: {data[:100]}")
+        self.io_stream.send(data, is_byte = is_byte)
+
+    def recv(self, is_byte = False):
+        data = self.io_stream.receive(is_byte=is_byte)
+        print(f"Received: {data[:100]}")
+        return data
+
+    def close(self):
+        self.io_stream.close()
 
 
 class Cloud_GUI(RawClient):
